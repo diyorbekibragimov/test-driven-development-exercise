43e2fdb64abb655c7fb3759e6fd11e56b23a9619
Diyorbek Ibragimov <dierbekibragimov@gmail.com>
2024-06-05 12:13:20 +0300

    test for last missing number

--- test_calculator.py
+++ test_calculator.py
@@ -12,10 +12,24 @@ Test Module for String Calculator
 import pytest
 from calculator import add
 
-
-
 def test_basic_add():
     """
         Tests addition. This test is written for you, extend it and add others! 
     """
     assert add("5,2") == "7", "Failed on 5+2==7"
+
+def test_missing_last_number():
+    """
+        Tests missing last number. 
+    """
+    assert ("5\n,") == "Number expected but EOF found."
+    assert add("5,") == "Number expected but EOF found."
+    assert add("5,,") == "Number expected but EOF found."
+    assert add("1,3,") == "Number expected but EOF found."
+    assert add("1,3,") == "Number expected but EOF found."
+    assert add("1,3,4,") == "Number expected but EOF found."
+    assert add(",") == "Number expected but EOF found."
+    assert add(",,") == "Number expected but EOF found."
+    assert add("1,,3") == "Number expected but EOF found."
+    assert add("1,3,4,") == "Number expected but EOF found."
+    assert add("1, 3, 4, ,") == "Number expected but EOF found."
